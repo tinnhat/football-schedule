6b14b4edd13b78f94c58dae3afa67043e758f887
Nguyễn Nhật Tín <60659593+tinnhat@users.noreply.github.com>
2025-10-21 18:53:55 +0700

    Fix code env serverless function

--- api/matches.js
+++ api/matches.js
@@ -1,21 +1,24 @@
+import process from 'process'
+
 export default async function handler(req, res) {
-  const { competition, dateFrom, dateTo } = req.query;
-  const token = import.meta.env.VITE_FOOTBALL_DATA_TOKEN; // 👉 Lưu key trong Vercel env
+  const { competition, dateFrom, dateTo } = req.query
+  
+  const token = process.env.FOOTBALL_DATA_TOKEN
 
   if (!competition || !dateFrom || !dateTo) {
-    return res.status(400).json({ message: 'Missing query parameters' });
+    return res.status(400).json({ message: 'Missing query parameters' })
   }
 
-  const url = `https://api.football-data.org/v4/competitions/${competition}/matches?dateFrom=${dateFrom}&dateTo=${dateTo}`;
+  const url = `https://api.football-data.org/v4/competitions/${competition}/matches?dateFrom=${dateFrom}&dateTo=${dateTo}`
 
   try {
     const response = await fetch(url, {
-      headers: { 'X-Auth-Token': token }
-    });
+      headers: { 'X-Auth-Token': token },
+    })
 
-    const data = await response.json();
-    return res.status(response.status).json(data);
+    const data = await response.json()
+    return res.status(response.status).json(data)
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message })
   }
 }
